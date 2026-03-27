d4ae619afe14f15eeaf35f74a4ed4fef87813b47
ItsKxteee <kate0212rodri@gmail.com>
2026-03-26 20:44:09 -0500

    superusuarioyano

--- build.sh
+++ build.sh
@@ -7,22 +7,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-#!/usr/bin/env bash
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-
-python manage.py migrate
-
-# Crear superusuario admin con contraseña fija sin pedir input
-python manage.py shell -c "
-from django.contrib.auth.models import User;
-user, created = User.objects.get_or_create(username='admin');
-user.email = 'admin@gmail.com';
-user.is_superuser = True;
-user.is_staff = True;
-user.set_password('TuContraseñaSegura');
-user.save();
-"
+python manage.py createsuperuser --noinput --username admin --email admin@gmail.com || true
